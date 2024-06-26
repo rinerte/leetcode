b119206eacffcd37b6216da089ba60f2578a3cb9
rinerte <rinerte@gmail.com>
2024-06-26 10:31:13 +0900

    add 138

---
+++ 138. Copy List with Random Pointer/Program.cs
@@ -0,0 +1,22 @@
+﻿public Node CopyRandomList(Node head) {
+        if(head == null) return null;
+
+            Dictionary<Node, Node> mapping = new Dictionary<Node, Node>();
+
+            Node current = head;
+            while (current != null)
+            {
+                mapping[current] = new(current.val);
+                current = current.next;
+            }
+            current = head;
+
+            while(current != null)
+            {
+                mapping[current].next = current.next != null ? mapping[current.next] : null;
+                mapping[current].random = current.random != null ? mapping[current.random] : null;
+                current = current.next;
+            }
+
+            return mapping[head];
+}
