92efca4f85062bdca607cc57d333dd4120fae5a4
rinerte <rinerte@gmail.com>
2024-06-26 10:11:34 +0900

    111 add

---
+++ 111. Minimum Depth of Binary Tree/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+public int MinDepth(TreeNode root) {
+        if (root == null) return 0;
+            Stack<(TreeNode node, int level) > stack = new Stack<(TreeNode node, int level)>();
+            stack.Push((root, 1));
+            int min = int.MaxValue;
+            while (stack.Any())
+            {
+                var node = stack.Pop();
+                if(node.level<min && node.node.left==null && node.node.right==null) min = node.level;
+                if (node.node.left != null) stack.Push((node.node.left, node.level + 1));
+                if (node.node.right != null) stack.Push((node.node.right, node.level + 1));
+            }
+            return min;        
+}
