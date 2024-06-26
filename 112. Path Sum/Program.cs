fc9d0e03bc9849ed6c8ec040c4adf9eb48bab2ad
rinerte <rinerte@gmail.com>
2024-06-26 10:15:43 +0900

    112 add

---
+++ 112. Path Sum/Program.cs
@@ -0,0 +1,9 @@
+﻿public bool HasPathSum(TreeNode root, int targetSum)
+{
+    if (root == null) return false;
+    int currentSum = root.val;
+
+    if(currentSum == targetSum && root.left==null && root.right==null) return true;
+
+    return (HasPathSum(root.left, targetSum - currentSum) || HasPathSum(root.right, targetSum - currentSum));
+}
