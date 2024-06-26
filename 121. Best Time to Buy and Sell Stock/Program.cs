fd7321cf2a8c632b17198da1a2f17b13e43fcc8c
rinerte <rinerte@gmail.com>
2024-06-26 10:22:12 +0900

    add 121

---
+++ 121. Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,13 @@
+﻿public int MaxProfit(int[] prices)
+{
+    if (prices.Length < 2) return 0;
+    int profit = 0;
+    int min = prices[0];
+
+    for(int i = 0; i < prices.Length; i++)
+    {
+        if(min>prices[i]) min = prices[i];
+        if (prices[i] - min > profit) profit = prices[i] - min;
+    }
+    return profit;
+}
