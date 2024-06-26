4cd19d8aff48758aee8bf3e3292b534d0eb98f64
rinerte <rinerte@gmail.com>
2024-06-26 10:18:54 +0900

    118 add

---
+++ 118. Pascal's Triangle/Program.cs
@@ -0,0 +1,18 @@
+﻿public IList<IList<int>> Generate(int numRows) {
+        IList<IList<int>> list = new List<IList<int>>();
+
+            for (int i = 0; i < numRows; i++)
+            {
+                List<int> row = new List<int>();
+                for(int j = 0; j < i+1; j++)
+                {
+                    if (j == 0 || j == i) row.Add(1);
+                    else
+                    {
+                        row.Add(list.Last()[j - 1] + list.Last()[j]);
+                    }
+                }
+                list.Add(row);
+            }
+            return list;
+}
