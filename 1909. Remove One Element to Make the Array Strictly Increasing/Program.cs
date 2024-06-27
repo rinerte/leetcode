68c1875db96908ca3eff162d26b8da81fb8c030c
rinerte <rinerte@gmail.com>
2024-06-27 13:44:57 +0900

    add 1909

---
+++ 1909. Remove One Element to Make the Array Strictly Increasing/Program.cs
@@ -0,0 +1,19 @@
+﻿public bool CanBeIncreasing(int[] nums) {
+        for (int i = 0; i < nums.Length; i++)
+            {
+                int[] newArr = nums.Where((v, n) => n != i).ToArray();
+                bool result = true;
+
+                for (int j = 1; j < newArr.Length; j++)
+                {
+                    if (newArr[j] <= newArr[j - 1])
+                    {
+                        result = false;
+                    }
+                }
+                if (result) return result;
+            }
+
+            return false;
+    }
+}
