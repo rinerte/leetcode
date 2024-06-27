a70b030c9674c38999ac28635f99d100c13388a9
rinerte <rinerte@gmail.com>
2024-06-27 13:32:39 +0900

    add 14

---
+++ 14. Longest Common Prefix/Program.cs
@@ -0,0 +1,20 @@
+﻿public string LongestCommonPrefix(string[] strs) {
+        int min = 200;
+            foreach (string str in strs)
+            {
+                if(str.Length <= min) min = str.Length;
+            }
+            StringBuilder sb = new("");
+
+            for(int i = 0; i < min; i++)
+            {
+                char c = strs[0][i];
+
+                foreach (string str in strs)
+                {
+                    if (str[i] != c) return sb.ToString();
+                }
+                sb.Append(c);
+            }
+            return sb.ToString();
+}
