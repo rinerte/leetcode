6752f383150cead8a3e3f53612b9a71b3d5a3b0e
rinerte <rinerte@gmail.com>
2024-06-27 13:29:55 +0900

    145 add

---
+++ 145. Binary Tree Postorder Traversal/Program.cs
@@ -0,0 +1,16 @@
+﻿public IList<int> PostorderTraversal(TreeNode root) {
+        IList<int> preorderTraversal = new List<int>();
+            if (root == null) return preorderTraversal;
+
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(root);
+
+            while (stack.Any())
+            {
+                var node = stack.Pop();
+                preorderTraversal.Insert(0, node.val);
+                if (node.left != null) stack.Push(node.left);
+                if (node.right != null) stack.Push(node.right);
+            }
+            return preorderTraversal;
+}
