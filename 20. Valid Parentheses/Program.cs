af9a94161c6a1c78ebaf807f7c6d9d1976e9d7df
rinerte <rinerte@gmail.com>
2024-06-27 13:58:17 +0900

    add 20

---
+++ 20. Valid Parentheses/Program.cs
@@ -0,0 +1,38 @@
+﻿public bool IsValid(string s) {
+        Dictionary<char, char> type = new Dictionary<char, char>()
+            {
+                {'(', ')'},
+                {'[', ']'},
+                {'{', '}' }
+            };
+            Dictionary<char, char> typeInverted = new Dictionary<char, char>()
+            {
+                {')', '('},
+                {']', '['},
+                {'}', '{' }
+            };
+
+            Stack<char> opened = new Stack<char>();
+            char popped;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (type.ContainsKey(s[i]))
+                {
+                    opened.Push(s[i]);
+                } else
+                {
+                    popped = '-';
+                    opened.TryPeek(out popped);
+                    if (typeInverted.GetValueOrDefault(s[i]) == popped)
+                    {              
+                        if (!opened.TryPop(out popped)) return false;
+                    } else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return !opened.TryPop(out popped);
+}
