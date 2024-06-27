53f00e27e7a10a816cfab0b2b2847b4b7fc7be03
rinerte <rinerte@gmail.com>
2024-06-27 13:55:32 +0900

    add 202

---
+++ 202. Happy Number/Program.cs
@@ -0,0 +1,17 @@
+﻿public bool IsHappy(int n) {
+        HashSet<int> sums = new HashSet<int>();
+
+            while (n!=1)
+            {
+                char[] chars = n.ToString().ToCharArray();
+                int sum = 0;
+                foreach(char c in chars)
+                {
+                    sum += (int)Math.Pow(Convert.ToInt32(c-48), 2);
+                }
+                n = sum;
+                if (sums.Contains(n)) return false;
+                sums.Add(n);
+            }
+            return true;
+}
