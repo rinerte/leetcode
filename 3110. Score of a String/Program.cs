5ff392ac4f1d15255cde3d4032e9e5d8be9e2fe2
rinerte <rinerte@gmail.com>
2024-06-27 14:54:51 +0900

    add 3110

---
+++ 3110. Score of a String/Program.cs
@@ -0,0 +1,8 @@
+﻿public int ScoreOfString(string s) {
+        byte[] asc = Encoding.ASCII.GetBytes(s);
+        int sum =0;
+        for(int i=1;i<asc.Length;i++){
+            sum+=Math.Abs(asc[i]-asc[i-1]);
+        }
+        return sum;
+}
