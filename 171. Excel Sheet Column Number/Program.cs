9b6bb55565218e2625ad3b55be59312074381559
rinerte <rinerte@gmail.com>
2024-06-27 13:42:27 +0900

    add 171

---
+++ 171. Excel Sheet Column Number/Program.cs
@@ -0,0 +1,10 @@
+﻿public int TitleToNumber(string columnTitle) {
+        int result = 1;
+
+            for(int i = 0; i < columnTitle.Length-1; i++)
+            {
+                result += (int)Math.Pow(26, columnTitle.Length - i - 1) * (columnTitle[i] - 'A' + 1);
+            }
+            result += (columnTitle[columnTitle.Length - 1] - 'A');
+            return result;
+}
