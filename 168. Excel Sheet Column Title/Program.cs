13839e7c861a82ccf9ca887e5d96fb3280464c32
rinerte <rinerte@gmail.com>
2024-06-27 13:37:41 +0900

    add 168

---
+++ 168. Excel Sheet Column Title/Program.cs
@@ -0,0 +1,11 @@
+﻿public string ConvertToTitle(int columnNumber) {
+        StringBuilder sb = new StringBuilder("");
+            while (columnNumber > 0)
+            {
+                columnNumber--;
+                char c = (char)('A' + columnNumber % 26);
+                columnNumber /= 26;
+                sb.Insert(0, c);
+            }
+            return sb.ToString();
+}   
