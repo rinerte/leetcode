39d97c25e151ca1f297f26324b4670d2290b2ac2
rinerte <rinerte@gmail.com>
2024-06-27 13:35:07 +0900

    add 160

---
+++ 160. Intersection of Two Linked Lists/Program.cs
@@ -0,0 +1,14 @@
+﻿public ListNode GetIntersectionNode(ListNode headA, ListNode headB) {
+        HashSet<ListNode> visited = new HashSet<ListNode>();
+            while (headA != null)
+            {
+                visited.Add(headA);
+                headA = headA.next;
+            }
+            while(headB != null)
+            {
+                if(visited.Contains(headB)) return headB;
+                headB = headB.next;
+            }
+            return null;
+    }
