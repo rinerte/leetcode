e2c2ba38178220e9a213b6cdd460c7914f593f17
rinerte <rinerte@gmail.com>
2024-06-27 13:39:56 +0900

    add 169

---
+++ 169. Majority Element/Program.cs
@@ -0,0 +1,18 @@
+﻿public int MajorityElement(int[] nums) {
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+            dict[0] = 0;
+            int max = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(nums[i]))
+                {
+                    dict[nums[i]]++;
+                }
+                else
+                {
+                    dict[nums[i]] = 1;
+                }
+                if (dict[nums[i]] > dict[max]) max = nums[i];
+            }
+            return max;
+}
