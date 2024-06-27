6996dc47b0c94731bba68293f213e6ba28fab90c
rinerte <rinerte@gmail.com>
2024-06-27 13:23:46 +0900

    add 141

---
+++ 141. Linked List Cycle/Program.cs
@@ -0,0 +1,13 @@
+﻿public bool HasCycle(ListNode head) {
+        if (head == null) return false;
+
+            while (head != null)
+            {
+                if (head.val == Int32.MaxValue) return true;
+
+                head.val = Int32.MaxValue;
+                head = head.next;
+            }
+
+            return false;
+}
