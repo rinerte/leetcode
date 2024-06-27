81391ef5891e7106c6207537bfbbcbb181affc1e
rinerte <rinerte@gmail.com>
2024-06-27 14:03:16 +0900

    add 242

---
+++ 242. Valid Anagram/Program.cs
@@ -0,0 +1,12 @@
+﻿public bool IsAnagram(string s, string t) {
+        if(s.Length!=t.Length) return false;
+            char[] sArray = s.ToCharArray();
+            char[] tArray = t.ToCharArray();
+            Array.Sort(sArray);
+            Array.Sort(tArray);
+            for(int i = 0; i < sArray.Length; i++)
+            {
+                if (tArray[i] != sArray[i]) return false;
+            }
+            return true;
+}
