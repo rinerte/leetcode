d1b7af4ae649f0e91d6a7e5db759d9e76e1d5a1d
rinerte <rinerte@gmail.com>
2024-06-27 14:41:36 +0900

    add 1791 beat 89% one line

--- 1791. Find Center of Star Graph/Program.cs
+++ 1791. Find Center of Star Graph/Program.cs
@@ -1,9 +1,3 @@
 ﻿public int FindCenter(int[][] edges) {
-        bool one = true;
-        bool two = true;
-        for(int i=1;i<edges.GetLength(0);i++){
-            if(edges[i][0]!=edges[0][0] && edges[i][1]!=edges[0][0]) one = false;
-            if(edges[i][0]!=edges[0][1] && edges[i][1]!=edges[0][1]) two = false;
-        }
-        return one?edges[0][0]:edges[0][1];
+        return edges[0][0]==edges[1][0]||edges[0][0]==edges[1][1]?edges[0][0]:edges[0][1];
 }
