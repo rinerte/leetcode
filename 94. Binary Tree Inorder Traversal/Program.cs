1b2788ae4eb0eac1a34f8dd2befbbe4801430054
rinerte <rinerte@gmail.com>
2024-06-28 15:14:39 +0900

    add 94

---
+++ 94. Binary Tree Inorder Traversal/Program.cs
@@ -0,0 +1,23 @@
+﻿public IList<int> InorderTraversal(TreeNode root) {
+    var list = new List<int>();
+    InorderTraverseIteratively(root, list);
+    return list;
+}
+    
+private void InorderTraverseIteratively(TreeNode root, IList<int> list)
+{
+    var current = root;
+    var stack = new Stack<TreeNode>();
+
+    while (current != null || stack.Count > 0)
+    {
+        while (current != null)
+        {
+            stack.Push(current);
+            current = current.left;
+        }
+        var top = stack.Pop();
+        list.Add(top.val);
+        current = top.right;
+    }
+}
