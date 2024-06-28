4170296ff403e3c8f38fb485253c6bcd00e2571b
rinerte <rinerte@gmail.com>
2024-06-28 15:12:17 +0900

    add 125

---
+++ 125. Valid Palindrome/Program.cs
@@ -0,0 +1,23 @@
+﻿public bool IsPalindrome(string s) {
+    int start = 0;
+    int end = s.Length-1;
+
+    while(start < end)
+    {
+        if (char.IsLetterOrDigit(s[start]) && char.IsLetterOrDigit(s[end]))
+        {
+            if (char.ToLower(s[start]) != char.ToLower(s[end])) return false;
+            else
+            {
+                start++;
+                end--;
+            }
+        }
+        else if (char.IsLetterOrDigit(s[start]))
+        {
+            end--;
+        }
+        else start++;
+    }
+    return true;
+}
