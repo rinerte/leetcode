8c85e960077b835fe1f8ad175c7147b562602569
rinerte <rinerte@gmail.com>
2024-06-28 14:42:20 +0900

    add 62

---
+++ 62. Unique Paths/Program.cs
@@ -0,0 +1,20 @@
+﻿public int UniquePaths(int m, int n) {
+    int[][] ar = new int[m][];
+        
+    for(int i = 0; i < m; i++)
+    {
+        ar[i] = new int[n];
+        ar[i][0] = 1;
+    }
+    Array.Fill(ar[0], 1);
+
+    for(int i = 1; i < m; i++)
+    {
+        for(int j = 1; j < n; j++)
+        {
+            ar[i][j] = ar[i - 1][j] + ar[i][j-1];
+        }
+    }
+
+    return ar[m-1][n-1];
+}
