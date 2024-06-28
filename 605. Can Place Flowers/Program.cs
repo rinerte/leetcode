22e2312300bafdb33da6ad27d0bacac1aafa00d6
rinerte <rinerte@gmail.com>
2024-06-28 14:40:19 +0900

    add 605

---
+++ 605. Can Place Flowers/Program.cs
@@ -0,0 +1,13 @@
+﻿public bool CanPlaceFlowers(int[] flowerbed, int n) {
+    int x = 0;
+
+    for (int i = 0; i < flowerbed.Length; i++)
+    {
+        if (flowerbed[i] == 0 && ((i + 1 == flowerbed.Length) || flowerbed[i + 1] == 0) && (i - 1 < 0 || flowerbed[i - 1] == 0))
+        {
+            x++;
+            flowerbed[i] = 1;
+        }
+    }
+    return x >= n;
+}
