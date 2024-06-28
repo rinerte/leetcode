f8307e37c570fd21eba1b766f0724405424ffbc3
rinerte <rinerte@gmail.com>
2024-06-28 15:10:41 +0900

    add 110

---
+++ 110. Balanced Binary Tree/Program.cs
@@ -0,0 +1,21 @@
+﻿public bool IsBalanced(TreeNode root) 
+{
+    return Height(root) >= 0;
+}
+
+public int Height(TreeNode node)
+{
+    if (node == null)
+    {
+        return 0;
+    }
+    int leftSubtreeHeight = Height(node.left);
+    if (leftSubtreeHeight == -1) return -1;
+    int rightSubtreeHeight = Height(node.right);
+    if (rightSubtreeHeight == -1) return -1;
+    if (Math.Abs(leftSubtreeHeight - rightSubtreeHeight) > 1)
+    {
+        return -1;
+    }
+    return (Math.Max(leftSubtreeHeight, rightSubtreeHeight) + 1);
+}
