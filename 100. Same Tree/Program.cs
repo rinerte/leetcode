bf2fbd2a902e88ce1bb4a56de841a120803f5795
rinerte <rinerte@gmail.com>
2024-06-28 15:04:47 +0900

    add 100

---
+++ 100. Same Tree/Program.cs
@@ -0,0 +1,7 @@
+﻿public bool IsSameTree(TreeNode p, TreeNode q) {
+    if (p?.val != q?.val || (q==null && p!=null) || (q!=null && p==null)) return false;
+    if (p == null && q == null) return true;
+    if(!IsSameTree(p.left, q.left)) return false;
+    if (!IsSameTree(p.right, q.right)) return false ;
+    return true;
+}
