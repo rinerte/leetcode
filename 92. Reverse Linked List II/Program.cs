67c38bf57613ad7a96de4a231fdf4d4e8e31ff03
rinerte <rinerte@gmail.com>
2024-06-28 15:03:00 +0900

    add 92

---
+++ 92. Reverse Linked List II/Program.cs
@@ -0,0 +1,29 @@
+﻿public ListNode ReverseBetween(ListNode head, int left, int right) {
+    int count = 1;
+    ListNode headPointer = head;
+    ListNode first=head, last = head.next;
+    Stack<ListNode> stack = new Stack<ListNode>();
+
+    if (head == null || left == right) return head;
+
+    ListNode dummyHead = new ListNode(0);
+    dummyHead.next = head;
+    ListNode previous = dummyHead;
+
+    for (int i = 0; i < left - 1; i++)
+    {
+        previous = previous.next;
+    }
+
+    ListNode current = previous.next;
+
+    for (int i = 0; i < right - left; i++)
+    {
+        ListNode nextNode = current.next;
+        current.next = nextNode.next;
+        nextNode.next = previous.next;
+        previous.next = nextNode;
+    }
+
+    return dummyHead.next;
+}
