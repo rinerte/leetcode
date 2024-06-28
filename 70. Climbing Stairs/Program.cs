1599b0925c1580c74abee79ed2a08d521a20370e
rinerte <rinerte@gmail.com>
2024-06-28 14:49:41 +0900

    add 70

---
+++ 70. Climbing Stairs/Program.cs
@@ -0,0 +1,11 @@
+﻿public int ClimbStairs(int n) {
+    if (n <= 3) return n;
+    List<int> fib = new();
+    fib.AddRange(new int[] { 0, 1, 2 });
+
+    for(int i = 3; i <= n; i++)
+    {
+        fib.Add(fib[i - 1] + fib[i - 2]);
+    }
+    return fib[n];
+}
