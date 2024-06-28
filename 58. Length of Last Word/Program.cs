a2ed454b780d9a00747bed0a7c7ea67d2dadcbc0
rinerte <rinerte@gmail.com>
2024-06-28 14:35:43 +0900

    add 58

---
+++ 58. Length of Last Word/Program.cs
@@ -0,0 +1,17 @@
+﻿public int LengthOfLastWord(string s) {
+    bool wordStarted = false;
+    int count = 0;
+
+    for(int i = s.Length - 1; i >= 0; i--)
+    {
+        if(s[i] != ' ')
+        {
+            wordStarted = true;
+            count++;
+        } else
+        {
+            if(wordStarted) return count;
+        }
+    }
+    return count;
+}
