a6a61fce4bbf051abe966fd5d24906f5ce67601e
rinerte <rinerte@gmail.com>
2024-06-28 14:28:57 +0900

    add 377

---
+++ 377. Combination Sum IV/Program.cs
@@ -0,0 +1,19 @@
+﻿public int CombinationSum4(int[] nums, int target) {
+    int[] dp = new int[target+1];
+        dp[0] = 1;
+
+    for(int i = 1; i < dp.Length; i++)
+    {
+        int sum = 0;
+        
+        foreach(int x in nums)
+        {
+            if(i-x>=0 && i-x<=i)
+            {
+                sum += dp[i - x];
+            }
+        }
+        dp[i] = sum;
+    }
+    return dp[target];
+}
