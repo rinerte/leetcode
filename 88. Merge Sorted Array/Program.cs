d11456a841a2136dcc7a2d7d17b47e745b09515b
rinerte <rinerte@gmail.com>
2024-06-28 14:59:39 +0900

    add 88

---
+++ 88. Merge Sorted Array/Program.cs
@@ -0,0 +1,6 @@
+﻿ public void Merge(int[] nums1, int m, int[] nums2, int n) {
+    var i = --m + n--;
+    
+    while (n >= 0)
+        nums1[i--] = m >= 0 && nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+}
