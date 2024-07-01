ceda7e414589992d0bc6cabd030dee8a24ee2321
rinerte <rinerte@gmail.com>
2024-07-01 12:01:07 +0900

    add 1550 beats 30% O(N)

---
+++ 1550. Three Consecutive Odds/Program.cs
@@ -0,0 +1,8 @@
+﻿public bool ThreeConsecutiveOdds(int[] arr) {
+    int count = 0;
+    for(int i = 0; i<arr.Length; i++){
+        if(arr[i]%2!=0) count++; else count =0;
+        if(count==3) return true;
+    }
+    return false;
+}
