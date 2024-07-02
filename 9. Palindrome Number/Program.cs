ab4aae5a32f4354b489546b05a301b44a3b98ab7
rinerte <rinerte@gmail.com>
2024-07-02 16:07:53 +0900

    beats 97% performance, 36% memory

---
+++ 9. Palindrome Number/Program.cs
@@ -0,0 +1,9 @@
+﻿public bool IsPalindrome(int x) {
+    var s = x.ToString().ToCharArray();
+
+    for(int i=0; i<s.Length/2;i++){
+        if(s[i]!=s[s.Length-1-i]) return false;
+    }
+    
+    return true;
+}
