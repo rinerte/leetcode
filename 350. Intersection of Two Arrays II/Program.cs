b77e922827d64b6974ea473bf5b7204caa08d166
rinerte <rinerte@gmail.com>
2024-07-02 14:55:36 +0900

    add 350

---
+++ 350. Intersection of Two Arrays II/Program.cs
@@ -0,0 +1,21 @@
+﻿public int[] Intersect(int[] nums1, int[] nums2) {
+    Dictionary<int,int> intCount = new();
+    
+    foreach(int i in nums1){
+        if(intCount.ContainsKey(i)){
+            intCount[i]++;
+        } else {
+            intCount[i] = 1;
+        }
+    }
+    
+    List<int> result = new();
+
+    foreach(int i in nums2){
+        if(intCount.ContainsKey(i)){
+            if(intCount[i]-->0) result.Add(i);
+        }
+    }
+
+    return result.ToArray();
+}
