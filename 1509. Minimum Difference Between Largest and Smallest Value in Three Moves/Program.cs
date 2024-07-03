4769ab09176a7f2ce998e135b5e7b83ca2b6b35e
rinerte <rinerte@gmail.com>
2024-07-03 13:55:55 +0900

    36% performance, 100% memory

---
+++ 1509. Minimum Difference Between Largest and Smallest Value in Three Moves/Program.cs
@@ -0,0 +1,10 @@
+﻿public int MinDifference(int[] nums) {
+    if(nums.Length<=4) return 0;
+    Array.Sort(nums);
+    
+    return Math.Min(Math.Min(
+        nums[nums.Length-4]-nums[0],nums[nums.Length-1]-nums[3]
+    ),Math.Min(
+        nums[nums.Length-3]-nums[1],nums[nums.Length-2]-nums[2]
+    ));
+}
