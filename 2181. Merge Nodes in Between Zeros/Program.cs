eee21d2474eda9c9f024b44b1a75b09100e91be2
rinerte <rinerte@gmail.com>
2024-07-04 09:47:24 +0900

    85% performance, 9% memory

---
+++ 2181. Merge Nodes in Between Zeros/Program.cs
@@ -0,0 +1,22 @@
+﻿public ListNode MergeNodes(ListNode head) {
+        
+    var current = new ListNode(0, null);
+    var dummy = current;
+
+    int sum = 0;
+
+    while(head!=null || sum>0){
+        if((int)head?.val==0){
+            if(sum>0){
+                current.next = new(sum,null);
+                current = current.next;
+            }
+            head = head.next;
+            sum = 0;
+        } else {
+            sum+=head.val;
+            head = head.next;
+        }
+    }
+    return dummy.next;
+}
