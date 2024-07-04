f676f09489892c7c55dd82e2f11ac3421e5246a6
rinerte <rinerte@gmail.com>
2024-07-04 15:59:45 +0900

    beats 73% runtime, 75% memory

---
+++ 263. Ugly Number/Program.cs
@@ -0,0 +1,16 @@
+﻿// An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+
+// Given an integer n, return true if n is an ugly number.
+
+public bool IsUgly(int n) {
+        int[] prime = new int[]{2,3,5};
+        if(n<=0) return false;
+        for(int i=0;i<3;i++){
+            if(n==1)return true;
+            if(n%prime[i]==0){
+                n/=prime[i];
+                i--;               
+            }
+        }
+        return false;
+    }
