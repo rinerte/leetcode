d8f5441d2fe0a37ef67c8d9b822e42ff483b2934
rinerte <rinerte@gmail.com>
2024-07-04 16:15:09 +0900

    runtime 47%

---
+++ 268. Missing Number/Program.cs
@@ -0,0 +1,9 @@
+﻿// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+public int MissingNumber(int[] nums) {        
+    int[] array = new int[nums.Length+1];
+    foreach(int n in nums){
+        array[n]=1;
+    }
+    for(int i=0;i<array.Length;i++) if(array[i]==0) return i;
+    return 0;
+}
