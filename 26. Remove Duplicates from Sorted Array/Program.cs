1b79f9325d8e18682e875846fba5b62012378c94
rinerte <rinerte@gmail.com>
2024-07-04 14:41:13 +0900

    15% runtime :(

---
+++ 26. Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,17 @@
+﻿public int RemoveDuplicates(int[] nums) {
+    int k = 0;
+    for(int i=1;i<nums.Length;i++){
+        if(nums[i]>nums[k]){
+            k++;
+            if(k<i){
+                for(int j=i;j<nums.Length;j++){
+                    int temp = nums[j];
+                    nums[j] = nums[j-i+k];
+                    nums[j-i+k] = temp;
+                }
+                i=k;
+            }
+        }
+    }
+    return k+1;
+}
