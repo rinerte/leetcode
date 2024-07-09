a00e14a97036fd4998315e3625f33f139d366218
rinerte <rinerte@gmail.com>
2024-07-09 10:22:43 +0900

    beats 78% runtime, 56% memory

--- 283. Move Zeroes/Program.cs
+++ 283. Move Zeroes/Program.cs
@@ -2,18 +2,12 @@
 
 // Note that you must do this in-place without making a copy of the array.
 public void MoveZeroes(int[] nums) {
+    int index =0;
     for(int i=0;i<nums.Length;i++){
-        if(nums[i]==0){
-            for(int j=i;j<nums.Length;j++){
-                if(nums[j]!=0){
-                    int temp = nums[j];
-                    nums[j]=nums[i];
-                    nums[i]=temp;
-                    i--;
-                    break;
-                }
-                
-            }
+        if(nums[i]!=0){
+            nums[index] = nums[i];
+            index++;
         }
     }
+    for(int i=1;i<nums.Length-index+1;i++) nums[^i]=0;
 }
