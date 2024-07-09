ea99be8519ef4651ae92b92ee575f671df7125ad
rinerte <rinerte@gmail.com>
2024-07-09 14:27:22 +0900

    OK GUD VERY WELL

---
+++ 345. Reverse Vowels of a String/Program.cs
@@ -0,0 +1,27 @@
+﻿// Given a string s, reverse only all the vowels in the string and return it.
+
+// The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+ 
+
+// Example 1:
+
+// Input: s = "hello"
+// Output: "holle"
+
+public string ReverseVowels(string s) {        
+    StringBuilder sb = new(s);
+    int i=0,j=s.Length-1;
+    while(i<=j){
+        if(isVowel(sb[i]) && isVowel(sb[j])){
+            var temp = sb[i];
+            sb[i] = sb[j];
+            sb[j] = temp;
+            i++; j--;
+        } else if(isVowel(sb[i])) j--; else i++;
+    }
+    return sb.ToString();
+}
+private bool isVowel(char s){
+    return "aeiou".Contains(char.ToLower(s));
+}
