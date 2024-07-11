6913a69382c2d4781f105c2ff0ce2b722dce5c97
rinerte <rinerte@gmail.com>
2024-07-11 15:03:20 +0900

    beats 99% runtime, 70% memory

---
+++ 3210. Find the Encrypted String/Program.cs
@@ -0,0 +1,11 @@
+﻿// You are given a string s and an integer k. Encrypt the string using the following algorithm:
+
+// For each character c in s, replace c with the kth character after c in the string (in a cyclic manner).
+// Return the encrypted string.
+public string GetEncryptedString(string s, int k) {
+    StringBuilder sb = new(s);
+    for(int i=0; i<s.Length;i++){
+        sb[i]=s[(i+k)%s.Length];
+    }
+    return sb.ToString();
+}
