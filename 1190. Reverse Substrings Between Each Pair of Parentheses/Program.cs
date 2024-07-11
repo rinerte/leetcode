a61451fe505e0fa8123726dad41eb32afc97ef16
rinerte <rinerte@gmail.com>
2024-07-11 10:33:06 +0900

    add not my solution

--- 1190. Reverse Substrings Between Each Pair of Parentheses/Program.cs
+++ 1190. Reverse Substrings Between Each Pair of Parentheses/Program.cs
@@ -7,35 +7,58 @@ using System.Text;
 
 Console.WriteLine(ReverseParentheses("(abcd)"));
 
-static string ReverseParentheses(string s) {
-    Stack<int> indexes = new();
-    StringBuilder sb = new(s);
+// static string ReverseParentheses(string s) {
+//     Stack<int> indexes = new();
+//     StringBuilder sb = new(s);
 
-    for(int i=0;i<s.Length;i++){
-        if(s[i]=='(')
-        indexes.Push(i);
-        else
-        if(s[i]==')'){
-            int k = indexes.Pop();
-            Console.WriteLine(" K is "+k);
-            int delta = 0;
-            for(int j=k;j<=k+(i-k)/2;j++){
-                var temp = sb[j];
-                sb[j]=sb[i-delta];
-                sb[i-delta]=temp;
-                delta++;
-                Console.WriteLine("J ="+j+"; k+(i-k)/2 = "+(k+(i-k)/2));
-                Console.WriteLine(sb.ToString() + "       ;");
-            }
+//     for(int i=0;i<s.Length;i++){
+//         if(s[i]=='(')
+//         indexes.Push(i);
+//         else
+//         if(s[i]==')'){
+//             int k = indexes.Pop();
+//             Console.WriteLine(" K is "+k);
+//             int delta = 0;
+//             for(int j=k;j<=k+(i-k)/2;j++){
+//                 var temp = sb[j];
+//                 sb[j]=sb[i-delta];
+//                 sb[i-delta]=temp;
+//                 delta++;
+//                 Console.WriteLine("J ="+j+"; k+(i-k)/2 = "+(k+(i-k)/2));
+//                 Console.WriteLine(sb.ToString() + "       ;");
+//             }
             
+//         }
+//     }
+//     int clear = 0;
+//     for(int i =0;i<sb.Length;i++){
+//         if(sb[i]!=')' && sb[i]!='('){
+//             sb[clear] = sb[i];
+//             clear++;
+//         }
+//     }
+//     return sb.ToString().Substring(0,clear);
+// }
+static string ReverseParentheses(string s) {
+    Stack<char> box = new Stack<char>();
+    Queue<char> box2= new Queue<char>();
+
+    foreach(var c in s)
+    {
+        if(c == ')')
+        {
+            while(box.Count != 0 && box.Peek() != '(') 
+                box2.Enqueue(box.Pop());
+            box.Pop();
+            while(box2.Count!= 0) box.Push(box2.Dequeue());
+        }
+        else
+        {
+            box.Push(c);
         }
     }
-    int clear = 0;
-    for(int i =0;i<sb.Length;i++){
-        if(sb[i]!=')' && sb[i]!='('){
-            sb[clear] = sb[i];
-            clear++;
-        }
-    }
-    return sb.ToString().Substring(0,clear);
+    StringBuilder result = new();
+    while(box.Count!= 0)
+        result.Insert(0,box.Pop());
+    return result.ToString();
 }
