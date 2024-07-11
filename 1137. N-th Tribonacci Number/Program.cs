d98d4f63f613515cb7c66ab82d268b9356133427
rinerte <rinerte@gmail.com>
2024-07-11 13:21:46 +0900

    beats 89% runtime, 64% memory

---
+++ 1137. N-th Tribonacci Number/Program.cs
@@ -0,0 +1,12 @@
+﻿// The Tribonacci sequence Tn is defined as follows: 
+
+// T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
+
+// Given n, return the value of Tn.
+public int Tribonacci(int n) {
+    int[] arr = new int[]{0,0,1};
+    for(int i=0;i<n;i++){
+        arr[i%3]=arr[0]+arr[1]+arr[2];
+    }
+    return arr[(n+1)%3];
+}
