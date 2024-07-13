046dfdb437683bc86b817d4b44f264385a44330f
rinerte <rinerte@gmail.com>
2024-07-13 12:30:05 +0900

    beats 81% runtime, 55% memory

---
+++ 643. Maximum Average Subarray I/Program.cs
@@ -0,0 +1,19 @@
+﻿//You are given an integer array nums consisting of n elements, and an integer k.
+
+//Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+public double FindMaxAverage(int[] nums, int k)
+{
+    double max = double.MinValue;
+    int sum = 0;
+    for (int i = 0; i < k; i++) sum += nums[i];
+    max = (double)sum / (double)k;
+
+    for (int i = k; i < nums.Length; i++)
+    {
+        sum -= nums[i - k];
+        sum += nums[i];
+        max = Math.Max((double)sum / (double)k, max);
+    }
+
+    return max;
+}
