c2eb0313d1b3525bd966b580f675738e5da44d35
rinerte <rinerte@gmail.com>
2024-07-14 13:53:37 +0900

    best possible solution, I guess, 75% runtime

---
+++ 136. Single Number/Program.cs
@@ -0,0 +1,8 @@
+﻿// Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+// You must implement a solution with a linear runtime complexity and use only constant extra space.
+public int SingleNumber(int[] nums) {
+    int s = nums[0];
+    for(int i=1;i<nums.Length;i++) s^=nums[i];
+    return s;
+}
