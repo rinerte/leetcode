75abc5dfd891c10e178d9c5e7140c5e9cdc91193
rinerte <rinerte@gmail.com>
2024-07-15 15:28:19 +0900

    cheated with LINQ, beats 76% runtime

---
+++ 2215. Find the Difference of Two Arrays/Program.cs
@@ -0,0 +1,9 @@
+﻿// Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
+
+// answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
+// answer[1] is a list of all distinct integers in nums2 which are not present in nums1.
+// Note that the integers in the lists may be returned in any order.
+
+public IList<IList<int>> FindDifference(int[] nums1, int[] nums2) {        
+    return new int[2][]{nums1.Distinct().Except(nums2).ToArray(),nums2.Distinct().Except(nums1).ToArray()};
+}
