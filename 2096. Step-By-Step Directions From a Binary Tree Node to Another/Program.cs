1e412193a649dbf90f552f6103c1a42b961a2b69
rinerte <rinerte@gmail.com>
2024-07-16 16:31:00 +0900

    it could work, but out of memory

--- 2096. Step-By-Step Directions From a Binary Tree Node to Another/Program.cs
+++ 2096. Step-By-Step Directions From a Binary Tree Node to Another/Program.cs
@@ -8,22 +8,34 @@
 // Return the step-by-step directions of the shortest path from node s to node t.
 public string GetDirections(TreeNode root, int startValue, int destValue) {
     Dictionary<int,string> paths= new();
-        Stack<TreeNode> stack = new();
-        stack.Push(root);
-        paths[root.val] = "";
+    Stack<TreeNode> stack = new();
+    stack.Push(root);
+    paths[root.val] = "";
 
-        while(stack.Any()){
-            var node = stack.Pop();
-            if(node.left!=null){
-                paths[node.left.val] = paths[node.val]+"L";
-                stack.Push(node.left);
-            }
-            if(node.right!=null){
-                paths[node.right.val] = paths[node.val]+"R";
-                stack.Push(node.right);
-            }
+    while(stack.Any()){
+        var node = stack.Pop();
+        if(node.left!=null){
+            paths[node.left.val] = paths[node.val]+"L";
+            stack.Push(node.left);
         }
-        result = paths[startValue] + paths[destValue];
-        return result.ToString();
+        if(node.right!=null){
+            paths[node.right.val] = paths[node.val]+"R";
+            stack.Push(node.right);
+        }
+    }
+    int min = Math.Min(paths[startValue].Length,paths[destValue].Length);
+    StringBuilder a = new(paths[startValue]);
+    StringBuilder b = new(paths[destValue]);
+    int count = 0;
 
+    for(int i=0;i<min;i++){
+        if(a[i]==b[i]){
+            count++;
+        } else break;
+    };
+    a.Remove(0,count);
+    b.Remove(0,count);
+    for(int i=0;i<a.Length;i++)a[i]='U';
+
+    return a.ToString()+b.ToString();
 }
