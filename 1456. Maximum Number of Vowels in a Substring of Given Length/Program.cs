48faad08a998d0ac09a1e3639f35245e97dc0ed4
rinerte <rinerte@gmail.com>
2024-07-18 11:07:47 +0900

    beats 86% runtime

---
+++ 1456. Maximum Number of Vowels in a Substring of Given Length/Program.cs
@@ -0,0 +1,21 @@
+﻿// Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+
+// Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+public int MaxVowels(string s, int k) {
+    int count =0;
+    for(int i=0;i<Math.Min(k,s.Length);i++){
+        if(isVowel(s[i])) count++;
+    }
+    int max = count;
+    
+    for(int i=k;i<s.Length;i++){
+        if(isVowel(s[i-k])) count--;
+        if(isVowel(s[i])) count++;
+        max = Math.Max(count,max);
+    }
+    return max;
+}
+
+bool isVowel(char c){
+    return "aeiou".Contains(c);
+}
