ba52c22e56e435f596a1f77c415386b7d3d6b5ab
rinerte <rinerte@gmail.com>
2024-07-18 15:54:06 +0900

    easy

---
+++ 1732. Find the Highest Altitude/Program.cs
@@ -0,0 +1,9 @@
+﻿public int LargestAltitude(int[] gain) {
+    int max =0;
+    int current =0;
+    for(int i=0;i<gain.Length;i++){
+        current+=gain[i];
+        if(max<current) max=current;
+    }
+    return max;
+}
