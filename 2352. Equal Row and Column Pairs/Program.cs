336f3f2e35c8b66304f2e5a0a6ca5b1187c69f87
rinerte <rinerte@gmail.com>
2024-07-22 15:37:31 +0900

    85% runtime, 88% memory

---
+++ 2352. Equal Row and Column Pairs/Program.cs
@@ -0,0 +1,15 @@
+﻿// Given a 0-indexed n x n integer matrix grid, return the number of pairs (ri, cj) such that row ri and column cj are equal.
+
+// A row and column pair is considered equal if they contain the same elements in the same order (i.e., an equal array).
+public int EqualPairs(int[][] grid) {
+    int count =0;
+    for(int row=0;row<grid.Length;row++){
+        for(int i=0;i<grid.Length;i++){
+            for(int j=0;j<grid.Length;j++){
+                if(grid[row][j]!=grid[j][i]) break;
+                if(j==grid.Length-1) count++;
+            }
+        }
+    }
+    return count;
+}
