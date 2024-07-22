e252031a44fed3a6310a510510a8f1e521612497
rinerte <rinerte@gmail.com>
2024-07-22 10:48:35 +0900

    78% runtime

---
+++ 2418. Sort the People/Program.cs
@@ -0,0 +1,10 @@
+﻿// You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
+
+// For each index i, names[i] and heights[i] denote the name and height of the ith person.
+
+// Return names sorted in descending order by the people's heights.
+
+public string[] SortPeople(string[] names, int[] heights) {
+    Array.Sort(heights,names,Comparer<int>.Create((x, y) => y.CompareTo(x)));
+    return names;
+}
