317b328734b6a88369725e245f2d710f261f0409
rinerte <rinerte@gmail.com>
2024-07-23 10:10:54 +0900

    beats 66% runtime, 100% memory

---
+++ 1636. Sort Array by Increasing Frequency/Program.cs
@@ -0,0 +1,25 @@
+﻿// Given an array of integers nums, sort the array in increasing order based on the frequency of the values. If multiple values have the same frequency, sort them in decreasing order.
+
+// Return the sorted array.
+Dictionary<int,int> d;
+
+public int[] FrequencySort(int[] nums) {
+    d = new();     
+    foreach(int i in nums){
+        if(!d.ContainsKey(i)){
+            d[i]=1;
+        } else d[i]++;
+    }
+    Array.Sort(nums,(x,y)=>MyComparer(x,y));
+    return nums;
+}
+
+int MyComparer(int x, int y){
+    if(d[x]>d[y]) return 1;
+    if (d[x]<d[y]) return -1;
+    else {
+        if(x>y) return -1;
+        if(x<y) return 1;
+    }
+    return 0;
+}
