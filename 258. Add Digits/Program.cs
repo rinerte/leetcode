a1b6f93c76886568e04ba75c5d0a1de6bb2ff1c8
rinerte <rinerte@gmail.com>
2024-07-27 14:04:57 +0900

    98%

---
+++ 258. Add Digits/Program.cs
@@ -0,0 +1,9 @@
+﻿//Given an integer num, repeatedly add all its digits until the result has only one digit, and return it.
+public int AddDigits(int num)
+{
+    while (num > 9)
+    {
+        num = num / 10 + num % 10;
+    }
+    return num;
+}
