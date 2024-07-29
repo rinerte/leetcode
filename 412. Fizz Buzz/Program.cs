e6f31f1d660b8da9f3b45567f003bcf88326282a
rinerte <rinerte@gmail.com>
2024-07-29 10:49:07 +0900

    50%

---
+++ 412. Fizz Buzz/Program.cs
@@ -0,0 +1,16 @@
+﻿// Given an integer n, return a string array answer (1-indexed) where:
+
+// answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+// answer[i] == "Fizz" if i is divisible by 3.
+// answer[i] == "Buzz" if i is divisible by 5.
+// answer[i] == i (as a string) if none of the above conditions are true.
+public IList<string> FizzBuzz(int n) {
+    string[] answer = new string[n];
+    for(int i =1;i<=n;i++){
+        if(i%5==0 && i%3==0) answer[i-1]="FizzBuzz";
+        else if(i%5==0) answer[i-1]="Buzz";
+        else if(i%3==0) answer[i-1]="Fizz";
+        else answer[i-1]=i.ToString();
+    }
+    return answer;
+}
