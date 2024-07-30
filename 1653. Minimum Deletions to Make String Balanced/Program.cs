f0ce554c084106fa5564b68bf1e0b6cad987985f
rinerte <rinerte@gmail.com>
2024-07-30 09:44:30 +0900

    runtime 50% memory 71%

---
+++ 1653. Minimum Deletions to Make String Balanced/Program.cs
@@ -0,0 +1,18 @@
+﻿// You are given a string s consisting only of characters 'a' and 'b'​​​​.
+
+// You can delete any number of characters in s to make s balanced. s is balanced if there is no pair of indices (i,j) such that i < j and s[i] = 'b' and s[j]= 'a'.
+
+// Return the minimum number of deletions needed to make s balanced.
+public int MinimumDeletions(string s) {
+    int count =0;
+    Stack<char> stack = new();
+
+    for(int i=0;i<s.Length;i++){
+        if(s[i]=='a' && stack.Any()){
+            count++;
+            stack.Pop();
+        } else
+        if(s[i]=='b') stack.Push('b');
+    }
+    return count;
+}
