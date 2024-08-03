c67ba311a7818b77354b9cf73c5d0c6d3f993e79
rinerte <rinerte@gmail.com>
2024-08-03 09:24:37 +0900

    45%

---
+++ 1460. Make Two Arrays Equal by Reversing Subarrays/Program.cs
@@ -0,0 +1,21 @@
+﻿//You are given two integer arrays of equal length target and arr. In one step, you can select any non-empty subarray of arr and reverse it. You are allowed to make any number of steps.
+
+//Return true if you can make arr equal to target or false otherwise.
+public bool CanBeEqual(int[] target, int[] arr)
+{
+    Dictionary<int, int> d = new();
+    for (int i = 0; i < target.Length; i++)
+    {
+        if (d.ContainsKey(target[i]))
+        {
+            d[target[i]]++;
+        }
+        else d[target[i]] = 1;
+        if (d.ContainsKey(arr[i]))
+        {
+            d[arr[i]]--;
+        }
+        else d[arr[i]] = -1;
+    }
+    return d.Values.Where(i => i != 0).Count() == 0;
+}
