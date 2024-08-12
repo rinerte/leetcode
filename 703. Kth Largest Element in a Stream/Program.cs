b332fb3bae70e95f700975f1a8f4c87a7140d010
rinerte <rinerte@gmail.com>
2024-08-12 10:03:53 +0900

    stupid solution

---
+++ 703. Kth Largest Element in a Stream/Program.cs
@@ -0,0 +1,21 @@
+﻿// Design a class to find the kth largest element in a stream. Note that it is the kth largest element in the sorted order, not the kth distinct element.
+
+// Implement KthLargest class:
+
+// KthLargest(int k, int[] nums) Initializes the object with the integer k and the stream of integers nums.
+// int add(int val) Appends the integer val to the stream and returns the element representing the kth largest element in the stream.
+public class KthLargest {
+    List<int> numsList;
+    int kk;
+    public KthLargest(int k, int[] nums) {        
+        Array.Sort(nums);
+        numsList = new(nums);
+        kk =k;
+    }
+    
+    public int Add(int val) {
+        numsList.Add(val);
+        numsList.Sort();
+        return numsList[^kk];
+    }
+}
