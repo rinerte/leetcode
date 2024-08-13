c101eea9e7ac80ad28a371dbbed0098a6e24baa7
rinerte <rinerte@gmail.com>
2024-08-13 15:44:38 +0900

    93%

--- 208. Implement Trie (Prefix Tree)/Program.cs
+++ 208. Implement Trie (Prefix Tree)/Program.cs
@@ -6,76 +6,49 @@
 // void insert(String word) Inserts the string word into the trie.
 // boolean search(String word) Returns true if the string word is in the trie (i.e., was inserted before), and false otherwise.
 // boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
-var t = new Trie();
-t.Insert("apple");
-Console.WriteLine(t.Search("apple"));
-Console.WriteLine(t.Search("app"));
-Console.WriteLine(t.StartsWith("app"));
-t.Insert("app");
-Console.WriteLine(t.Search("app"));
-Console.WriteLine(t.Search("apple"));
-
 public class Trie {
-    Node root;
+//97
+    bool[] values;
+    public Trie[] next;
+    Trie root;
+    public bool endWord;
+
     public Trie() {
-        root = new();
+        root = this;
+        next = new Trie[26];
+        values = new bool[26];        
     }
     
     public void Insert(string word) {
         var head = root;
-        for(int i=0;i<word.Length-1;i++){
-            if(head[word[i]-97]==true) continue;
-            head[word[i]-97]=true;
-            head.next = head.next ?? new();
-            head = head.next;
+        for(int i=0;i<word.Length;i++){
+            head.values[word[i]-97] = true;
+            head.next[word[i]-97] = head.next[word[i]-97] ?? new();
+            head = head.next[word[i]-97];
         }
-        head[word[word.Length-1]-97]=true;
-        head.SetEnd(word[word.Length-1]-97);
+        head.endWord = true;
     }
     
     public bool Search(string word) {
         var head = root;
-        
-        for(int i=0;i<word.Length-1;i++){
-            if(head[word[i]-97]==false) return false;
-            if(head.next == null) return false;
-            head = head.next;
-            Console.WriteLine("Search with index = "+(word[i]-97));
+        for(int i=0;i<word.Length;i++){
+            if(head==null || head?.values[word[i]-97] == false) return false;
+            head = head.next[word[i]-97];
         }
-        Console.WriteLine("Search with index = "+ (word[word.Length-1]-97));
-        if(head[word[word.Length-1]-97]==false) return false;
-        Console.WriteLine("Search Ended");
-        return (head.End(word[word.Length-1]-97));
+        return head.endWord;
     }
     
     public bool StartsWith(string prefix) {
         var head = root;
         for(int i=0;i<prefix.Length;i++){
-            if(head[prefix[i]-97]==false) return false;
-            if(head.next == null) return false;
-            head = head.next;
+            if(head==null || head?.values[prefix[i]-97] == false) return false;
+            head = head.next[prefix[i]-97];
         }
         return true;
     }
-}
-public class Node{
-    public bool[] letters;
-    public bool[] wordEnd;
-    public Node next;
-    public Node(){
-        letters = new bool[26];
-        wordEnd = new bool[26];      
-    }
-    public bool End(int index){
-        return wordEnd[index];
-    }
-    public void SetEnd(int index){
-        //Console.WriteLine("Set end with index ="+index+" to TRUE");
-        wordEnd[index]=true;
-    }
     public bool this[int i]
     {
-        get { return letters[i]; }
-        set { letters[i] = value; }
+        get { return values[i]; }
+        set { values[i] = value; }
     }
 }
