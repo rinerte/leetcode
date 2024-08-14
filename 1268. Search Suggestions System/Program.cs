0ae96c2286b4f8e24ca2636fa08b03830d3653ba
rinerte <rinerte@gmail.com>
2024-08-14 14:08:39 +0900

    30%

---
+++ 1268. Search Suggestions System/Program.cs
@@ -0,0 +1,89 @@
+﻿// You are given an array of strings products and a string searchWord.
+
+// Design a system that suggests at most three product names from products after each character of searchWord is typed. Suggested products should have common prefix with searchWord. If there are more than three products with a common prefix return the three lexicographically minimums products.
+
+// Return a list of lists of the suggested products after each character of searchWord is typed.
+
+public class Solution {
+
+    public IList<IList<string>> SuggestedProducts(string[] products, string searchWord) {
+        IList<IList<string>> list = new List<IList<string>>();
+        Trie t = new();
+        foreach(var p in products) t.Insert(p);
+
+        for(int i=1;i<=searchWord.Length;i++){
+            list.Add(t.Suggest(searchWord.Substring(0,searchWord.Length-(searchWord.Length-i))));
+        }
+        return list;
+    }
+    
+}
+public class Trie {
+//97
+    bool[] values;
+    public Trie[] next;
+    Trie root;
+    public bool endWord;
+
+    public Trie() {
+        root = this;
+        next = new Trie[26];
+        values = new bool[26];        
+    }
+    public IList<string> Suggest(string prefix){
+        IList<string> list = new List<string>();
+        var head = root;
+        for (int i = 0; i < prefix.Length; i++) {
+            if (head == null || head?.values[prefix[i] - 97] == false) return list;
+            head = head.next[prefix[i] - 97];
+        }
+        PriorityQueue<(Trie trie, string word), string> pq = new();
+        pq.Enqueue((head, prefix), prefix);
+
+        while (pq.Count > 0 && list.Count < 3) {
+            var (trie, word) = pq.Dequeue();
+            if (trie.endWord) {
+                list.Add(word);
+            }
+            for (int i = 0; i < 26; i++) {
+                if (trie.next[i] != null) {
+                    string newWord = word + (char)(i + 97);
+                    pq.Enqueue((trie.next[i], newWord), newWord);
+                }
+            }
+        }
+        return list;
+    }
+    public void Insert(string word) {
+        var head = root;
+        for(int i=0;i<word.Length;i++){
+            head.values[word[i]-97] = true;
+            head.next[word[i]-97] = head.next[word[i]-97] ?? new();
+            head = head.next[word[i]-97];
+        }
+        head.endWord = true;
+    }
+    
+    public bool Search(string word) {
+        var head = root;
+        for(int i=0;i<word.Length;i++){
+            if(head==null || head?.values[word[i]-97] == false) return false;
+            head = head.next[word[i]-97];
+        }
+        return head.endWord;
+    }
+    
+    public bool StartsWith(string prefix) {
+        var head = root;
+        for(int i=0;i<prefix.Length;i++){
+            if(head==null || head?.values[prefix[i]-97] == false) return false;
+            head = head.next[prefix[i]-97];
+        }
+        return true;
+    }
+    public bool this[int i]
+    {
+        get { return values[i]; }
+        set { values[i] = value; }
+    }
+}
--- 719. Find K-th Smallest Pair Distance/Program.cs
+++ 719. Find K-th Smallest Pair Distance/Program.cs
@@ -2,28 +2,55 @@
 
 // Given an integer array nums and an integer k, return the kth smallest distance among all the pairs nums[i] and nums[j] where 0 <= i < j < nums.length.
 
-public int SmallestDistancePair(int[] nums, int k) {
-    int[] smallest = new int[k];
-    Array.Fill(smallest, int.MaxValue);
+// INEFICIENT
+// public int SmallestDistancePair(int[] nums, int k) {
+//     int[] smallest = new int[k];
+//     Array.Fill(smallest, int.MaxValue);
 
-    int n = nums.Length;        
+//     int n = nums.Length;        
 
-    for(int i=0; i<n; i++) {
-        for(int j=i+1;j<n;j++){
-            int d = Math.Abs(nums[j]-nums[i]);
+//     for(int i=0; i<n; i++) {
+//         for(int j=i+1;j<n;j++){
+//             int d = Math.Abs(nums[j]-nums[i]);
             
-            int maxIndex = 0;
-            for (int l = 1; l < k; l++) {
-                if (smallest[l] > smallest[maxIndex]) {
-                    maxIndex = l;
-                }
-            }
-            if (d < smallest[maxIndex]) {
-                smallest[maxIndex] = d;
-            }
-        }
-    }
+//             int maxIndex = 0;
+//             for (int l = 1; l < k; l++) {
+//                 if (smallest[l] > smallest[maxIndex]) {
+//                     maxIndex = l;
+//                 }
+//             }
+//             if (d < smallest[maxIndex]) {
+//                 smallest[maxIndex] = d;
+//             }
+//         }
+//     }
 
-    Array.Sort(smallest);
-    return smallest[k-1];
+//     Array.Sort(smallest);
+//     return smallest[k-1];
+// }
+public int SmallestDistancePair(int[] nums, int k) {
+    Array.Sort(nums);    
+    int left = 0;
+    int right = nums[nums.Length - 1] - nums[0];
+    
+    while (left < right) {
+        int mid = left + (right - left) / 2;
+        if (CountPairs(nums, mid) < k) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+    }        
+    return left;
+}
+private int CountPairs(int[] nums, int maxDist) {
+    int count = 0;
+    int j = 0;
+    for (int i = 0; i < nums.Length; i++) {
+        while (j < nums.Length && nums[j] - nums[i] <= maxDist) {
+            j++;
+        }
+        count += j - i - 1;
+    }
+    return count;
 }
