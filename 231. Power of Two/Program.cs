04a71c65b282d478265433c64a01723e6941e0b2
rinerte <rinerte@gmail.com>
2024-08-20 08:39:09 +0900

    21%

---
+++ 231. Power of Two/Program.cs
@@ -0,0 +1,10 @@
+﻿// Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+// An integer n is a power of two, if there exists an integer x such that n == 2x.
+public bool IsPowerOfTwo(int n) {
+    while (n>1){
+        if((int)(n & 1) == 1) return false;
+        n= n >> 1;
+    }
+    return n == 1;
+}
