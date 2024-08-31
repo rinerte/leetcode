cc2bfab3280515337885bef16ca3eeb35b73e918
rinerte <rinerte@gmail.com>
2024-08-31 20:49:15 +0900

    80%

---
+++ 2022. Convert 1D Array Into 2D Array/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int[][] Construct2DArray(int[] original, int m, int n)
+    {
+        int[][] result = new int[m][];
+
+        switch (m * n == original.Length ? 1 : 0)
+        {
+            case 1:
+                int i = 0;
+                while (i < m)
+                {
+                    result[i] = new int[n];
+                    Array.Copy(original, i * n, result[i], 0, n);
+                    i++;
+                }
+                break;
+            default:
+                return new int[0][];
+        }
+
+        return result;
+    }
+}
