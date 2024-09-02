dbd0544fabedd83e3f425869be46ae1689444bc0
rinerte <rinerte@gmail.com>
2024-09-03 00:30:32 +0900

    -

---
+++ 1894. Find the Student that Will Replace the Chalk/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Solution
+{
+    public int ChalkReplacer(int[] chalk, int k)
+    {
+        long totalChalkNeeded = 0;
+        foreach (int studentChalkUse in chalk)
+        {
+            totalChalkNeeded += studentChalkUse;
+        }
+
+        int remainingChalk = (int)(k % totalChalkNeeded);
+
+        for (int studentIndex = 0; studentIndex < chalk.Length; studentIndex++)
+        {
+            if (remainingChalk < chalk[studentIndex])
+            {
+                return studentIndex;
+            }
+            remainingChalk -= chalk[studentIndex];
+        }
+
+        return 0;
+    }
+}
