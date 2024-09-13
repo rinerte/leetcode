0664b9b435caa7132f20bc5d4cbc98beb199e958
rinerte <rinerte@gmail.com>
2024-09-13 09:46:28 +0900

    optimized, prefix sum, 40%

--- 1310. XOR Queries of a Subarray/Program.cs
+++ 1310. XOR Queries of a Subarray/Program.cs
@@ -3,14 +3,31 @@
 // For each query i compute the XOR of elements from lefti to righti (that is, arr[lefti] XOR arr[lefti + 1] XOR ... XOR arr[righti] ).
 
 // Return an array answer where answer[i] is the answer to the ith query.
+// public class Solution {
+//     public int[] XorQueries(int[] arr, int[][] queries) {
+//         int[] ans = new int[queries.Length];
+//         for(int i=0;i<queries.Length;i++){
+//             int res = arr[queries[i][0]];
+//             for(int j=queries[i][0]+1;j<=queries[i][1];j++) res^=arr[j];
+//             ans[i] = res;
+//         }
+//         return ans;
+//     }
+// }
 public class Solution {
     public int[] XorQueries(int[] arr, int[][] queries) {
-        int[] ans = new int[queries.Length];
-        for(int i=0;i<queries.Length;i++){
-            int res = arr[queries[i][0]];
-            for(int j=queries[i][0]+1;j<=queries[i][1];j++) res^=arr[j];
-            ans[i] = res;
+        int[] prefixSum = new int[arr.Length+1];
+        for(int i = 1; i <= arr.Length; i++)
+        {
+            prefixSum[i] = prefixSum[i-1] ^ arr[i-1]; 
         }
-        return ans;
+
+        int[] result = new int[queries.Length];
+
+        for(int i = 0; i<queries.Length; i++)
+        {
+            result[i] = prefixSum[queries[i][0]] ^ prefixSum[queries[i][1]+1];
+        }
+        return result;
     }
 }
