a7322177b835e5bf308656934ba3c14e029e387e
rinerte <rinerte@gmail.com>
2024-09-18 09:33:11 +0900

    99%

---
+++ 179. Largest Number/Program.cs
@@ -0,0 +1,12 @@
+﻿// Given a list of non-negative integers nums, arrange them such that they form the largest number and return it.
+
+// Since the result may be very large, so you need to return a string instead of an integer.
+public class Solution {
+    public string LargestNumber(int[] nums) {        
+        var numStrings = nums.Select(n => n.ToString()).ToArray();
+        Array.Sort(numStrings, (x, y) => (y + x).CompareTo(x + y));
+        if (numStrings[0] == "0") return "0";
+        return string.Join("", numStrings);    
+    }
+    
+}
