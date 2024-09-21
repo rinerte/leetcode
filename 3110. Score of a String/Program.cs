dba10c7f2b41bb58e98e000b1ade38bf54d74897
rinerte <rinerte@gmail.com>
2024-09-21 09:28:56 +0900

    add desc

--- 3110. Score of a String/Program.cs
+++ 3110. Score of a String/Program.cs
@@ -1,4 +1,7 @@
-﻿public int ScoreOfString(string s) {
+﻿// You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+// Return the score of s.
+
+public int ScoreOfString(string s) {
         byte[] asc = Encoding.ASCII.GetBytes(s);
         int sum =0;
         for(int i=1;i<asc.Length;i++){
