f8535de92631311c01a543ef35cf7ae85a2eb7c6
rinerte <rinerte@gmail.com>
2024-09-22 09:31:14 +0900

    add description

--- 2181. Merge Nodes in Between Zeros/Program.cs
+++ 2181. Merge Nodes in Between Zeros/Program.cs
@@ -1,4 +1,10 @@
-﻿public ListNode MergeNodes(ListNode head) {
+﻿// You are given the head of a linked list, which contains a series of integers separated by 0's. The beginning and end of the linked list will have Node.val == 0.
+
+// For every two consecutive 0's, merge all the nodes lying in between them into a single node whose value is the sum of all the merged nodes. The modified list should not contain any 0's.
+
+// Return the head of the modified linked list.
+
+public ListNode MergeNodes(ListNode head) {
         
     var current = new ListNode(0, null);
     var dummy = current;
