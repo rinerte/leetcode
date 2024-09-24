79909692282cdefe44540689ab0580ce224d40ac
rinerte <rinerte@gmail.com>
2024-09-24 09:16:56 +0900

    time exceeded

---
+++ 3043. Find the Length of the Longest Common Prefix/Program.cs
@@ -0,0 +1,27 @@
+﻿// You are given two arrays with positive integers arr1 and arr2.
+
+// A prefix of a positive integer is an integer formed by one or more of its digits, starting from its leftmost digit. For example, 123 is a prefix of the integer 12345, while 234 is not.
+
+// A common prefix of two integers a and b is an integer c, such that c is a prefix of both a and b. For example, 5655359 and 56554 have a common prefix 565 while 1223 and 43456 do not have a common prefix.
+
+// You need to find the length of the longest common prefix between all pairs of integers (x, y) such that x belongs to arr1 and y belongs to arr2.
+
+// Return the length of the longest common prefix among all pairs. If no common prefix exists among them, return 0.
+public class Solution {
+    public int LongestCommonPrefix(int[] arr1, int[] arr2) {
+        int length =0;
+        for(int i=0;i<arr1.Length;i++){
+            for(int j=0;j<arr2.Length;j++){
+                int count = 0;
+                var first = arr1[i].ToString();
+                var second = arr2[j].ToString();
+                int l = first.Length<=second.Length?first.Length:second.Length;
+                for(int k=0;k<l;k++){
+                    if(first[k]==second[k])count++; else break;
+                }
+                if(count>length) length = count;
+            }
+        }
+        return length;
+    }
+}
