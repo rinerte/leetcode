8168ffe5999c54909990e3e7acd8127deba03ff9
rinerte <rinerte@gmail.com>
2024-10-06 10:10:02 +0900

    add desc

--- 2696. Minimum String Length After Removing Substrings/Program.cs
+++ 2696. Minimum String Length After Removing Substrings/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// You are given a string s consisting only of uppercase English letters.
+
+// You can apply some operations to this string where, in one operation, you can remove any occurrence of one of the substrings "AB" or "CD" from s.
+
+// Return the minimum possible length of the resulting string that you can obtain.
+
+// Note that the string concatenates after removing the substring and could produce new "AB" or "CD" substrings.
