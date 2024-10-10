2d482e44b0add38c7aa9eba014453c79126edd56
rinerte <rinerte@gmail.com>
2024-10-10 10:14:41 +0900

    monotonic stack

--- 962. Maximum Width Ramp/Program.cs
+++ 962. Maximum Width Ramp/Program.cs
@@ -1,3 +1,19 @@
 ﻿// A ramp in an integer array nums is a pair (i, j) for which i < j and nums[i] <= nums[j]. The width of such a ramp is j - i.
 
-// Given an integer array nums, return the maximum width of a ramp in nums. If there is no ramp in nums, return 0.
+// Given an integer array nums, return the maximum width of a ramp in nums. If there is no ramp in nums, return 0.public class Solution {
+    public int MaxWidthRamp(int[] nums) {
+        Stack<int> stack = new();
+        for(int i=0;i<nums.Length; i++){
+            if(!stack.Any() || nums[stack.Peek()]>nums[i]) stack.Push(i);
+        }
+        int maxW =0;
+
+        for(int i = nums.Length-1;i>=0;i--){
+            while(stack.Any() && nums[stack.Peek()]<=nums[i]){
+                maxW = Math.Max(maxW, i - stack.Peek());
+                stack.Pop();
+            }
+        }
+        return maxW;
+    }
+}
