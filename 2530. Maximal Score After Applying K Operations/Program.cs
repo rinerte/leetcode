69f4941587f541eeff3c9beb4eee15f187a16e11
rinerte <rinerte@gmail.com>
2024-10-14 10:58:34 +0900

    priority queue

---
+++ 2530. Maximal Score After Applying K Operations/Program.cs
@@ -0,0 +1,23 @@
+﻿// You are given a 0-indexed integer array nums and an integer k. You have a starting score of 0.
+
+// In one operation:
+
+// choose an index i such that 0 <= i < nums.length,
+// increase your score by nums[i], and
+// replace nums[i] with ceil(nums[i] / 3).
+// Return the maximum possible score you can attain after applying exactly k operations.
+
+// The ceiling function ceil(val) is the least integer greater than or equal to val.
+public class Solution {
+    public long MaxKelements(int[] nums, int k) {
+        PriorityQueue<int,int> queue = new(nums.Select(num=>(num,-num)));
+        long score =0;
+        for(int i=0;i<k;i++){
+            int number = queue.Dequeue();
+            score+=number;
+            number = number % 3 == 0 ? number/3 : number / 3 + 1;
+            queue.Enqueue(number,-number);
+        }
+        return score;
+    }
+}
